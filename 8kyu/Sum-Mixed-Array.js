7dbeee4f9419a8875de9c2f5e526450f967674b8
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-12 20:22:10 +0100

    Sum Mixed Array

---
+++ 8kyu/Sum-Mixed-Array.js
@@ -0,0 +1,15 @@
+function sumMix(x){
+    y = x.map(function(x) {
+      return (typeof x) == 'string' ? parseInt(x) : x
+    });
+    
+    const r = (acc, cur) => acc + cur
+    return y.reduce(r)
+  }
+
+
+//   https://www.codewars.com/kata/sum-mixed-array/train/javascript
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
