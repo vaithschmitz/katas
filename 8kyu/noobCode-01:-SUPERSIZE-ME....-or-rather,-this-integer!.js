a4c4d7984f3177e4c0a30636142a5a4d5f027b64
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-14 01:36:33 +0100

    noobCode 01: SUPERSIZE ME.... or rather, this integer!

---
+++ 8kyu/noobCode-01:-SUPERSIZE-ME....-or-rather,-this-integer!.js
@@ -0,0 +1,13 @@
+function superSize(num){
+    return parseInt(num.toString().split('').sort().reverse().join(''))
+  }
+
+
+//   https://www.codewars.com/kata/noobcode-01-supersize-me-dot-dot-dot-or-rather-this-integer/train/javascript
+
+// Write a function that rearranges an integer into its largest possible value.
+
+// superSize(123456) //654321
+// superSize(105) // 510
+// superSize(12) // 21
+// If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
