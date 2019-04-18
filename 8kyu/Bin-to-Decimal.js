1902a468ecef95abd258e6bcdc59acf45a3cfbaa
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-19 00:08:53 +0100

    Bin to Decimal

---
+++ 8kyu/Bin-to-Decimal.js
@@ -0,0 +1,8 @@
+function binToDec(bin){
+    return parseInt(bin, 2) // the second arg of parseInt specifies radix/base -> base 2 is binary
+  }
+
+
+
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
