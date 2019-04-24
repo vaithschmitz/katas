e36fc7d6db6e65d80a9656cd54b850b6a35959ad
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-04-24 01:21:43 +0100

    Remove duplicates from list

---
+++ 8kyu/Remove-duplicates-from-list.js
@@ -0,0 +1,8 @@
+const distinct = (a) => {
+    return Array.from( new Set([... a]))
+  }
+
+// https://www.codewars.com/kata/remove-duplicates-from-list/train/javascript
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
