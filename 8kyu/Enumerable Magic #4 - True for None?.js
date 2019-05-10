23614f74032954e97fc8de0a9caad02153a81ec6
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-05-10 01:18:43 +0100

    Enumerable Magic #4 - True for None?

---
+++ 8kyu/Enumerable Magic #4 - True for None?.js
@@ -0,0 +1,8 @@
+function none(arr, fun){
+    return !arr.some(fun)
+  }
+
+
+// https://www.codewars.com/kata/enumerable-magic-number-4-true-for-none/train/javascript
+
+// Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.Create a method none? (JS none) that accepts an array and a block (JS: a function), and returns true if the block (/function) returns true for none of the items in the array. An empty list should return true.
