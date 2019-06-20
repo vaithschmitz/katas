e79841ffb6eb3eaebe26b4705179bd9f3be50870
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-21 00:27:56 +0100

    Find the Integral

---
+++ 8kyu/Find-the-Integral.js
@@ -0,0 +1,16 @@
+function integrate(c, e) {
+    return `${c / (e+1)}x^${e +1}`
+  }
+
+
+// https://www.codewars.com/kata/find-the-integral/train/javascript
+
+// Create a function that finds the integral of the expression passed.
+
+// In order to find the integral all you need to do is add one to the exponent (the second number), and divide the coefficient by that new number (the first number).
+
+// In 3x^2, for example, the integral would be 1x^3 (we added 1 to the exponent, and divided the coefficient by that new number).
+
+// integrate(3,2) // => "1x^3"
+// integrate(9,5) // => "1.5x^6"
+// Note that the output should be a string. The coefficient is always positive. Neither number will ever be 0.
